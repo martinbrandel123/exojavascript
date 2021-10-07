b201f98df248a5106412ba9372e7353c2d147bc5
brandel martin <martin.brandel123@gmail.com>
2021-10-07 17:54:06 +0200

    exo finish

--- exo CreatePassword/app.js
+++ exo CreatePassword/app.js
@@ -1,33 +1,34 @@
+// var data
 const dataLowerCase = "abcdefghijklmnopqrstuvwxyz";
 const dataUpperCase =  dataLowerCase.toUpperCase();
 const dataNumbers = "012345678901234567890123456789";
 const dataSymbols = "&é'(-è_çà)=/*-+!:;,?./§$£¨^µ%*ù"
 
-//let number = 
-
-
-
-
-
-
-
-// DOM VAR 
+//var DOM
 let btn = document.getElementById('submit_btn')
+let inp = document.getElementById('number')
+let password = document.getElementById('password')
+
+
+//function 
 function createPassword () {
     let globalArray = []
     globalArray.push(...dataLowerCase)
     globalArray.push(...dataUpperCase)
     globalArray.push(...dataNumbers)
     globalArray.push(...dataSymbols)
-
-    let mixedArray = globalArray.sort(()=> Math.random() - 0.5)
-         console.log(mixedArray)   
+    let mixedArray = globalArray
+                    .sort(()=> Math.random() - 0.5)
+                    .splice(1, inp.value)
+                    .join("")
+    password.innerHTML = mixedArray
 }
 
 
 
 
 
+// listener
 btn.addEventListener('click', createPassword)
 
 
